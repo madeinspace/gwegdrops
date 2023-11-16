05e6ad3741836490f0b681cc243068777f138a0f
Fabrice Million <fabricemillion@fabrices-mini.lan>
2023-11-16 13:10:39 +1100

    Feat: added youtube video ids

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,13 @@ import { useMemo } from 'react';
 import styles from './page.module.css'
 
 const DROPS = [
-  {id: 1, name:'Greenwood Island', date:new Date('2023, 9, 11'), media:'Mj-CtgMtmjk',  lat: -12.625240, lng: 130.949268},
-  {id: 2, name:'Airyes Inlet', date:new Date('2023, 11, 27'), media:'LOvSxWMm1hw', lat:-38.467995, lng: 144.104330}, 
-  {id: 3, name:'Secret Beach - Poda Island', date:new Date('2023, 12, 18'), media:'s8jScp-ENlY', lat:7.970540, lng:98.805951},
-  {id: 4, name:'Kata Beach', date:new Date('2023, 9, 23'), media:'jqUBCqGNq6k', lat: 7.813757, lng:98.298382}]
+  {id: 1, name:'Greenwood Island', date:new Date('2023, 8, 27'), media:'5OVRmLpkP1E',  lat: -12.625240, lng: 130.949268},
+  {id: 2, name:'Airyes Inlet', date:new Date('2023, 9, 13'), media:'KtO8zl2eISI', lat:-38.467995, lng: 144.104330}, 
+  {id: 3, name:'Secret Beach - Poda Island', date:new Date('2023, 10, 22'), media:'ntJfb8BXAyE', lat:7.970540, lng:98.805951},
+  {id: 4, name:'Kata Beach', date:new Date('2023, 10, 26'), media:'vgouERzN7ik', lat: 7.813757, lng:98.298382}]
  
 
 export default function GWEG() {
-
-
 
   const Map = useMemo(() => dynamic(
     () => import('./components/Map'),
@@ -21,7 +19,6 @@ export default function GWEG() {
       ssr: false
     }
   ), [])
-
 
   return (
     <main className={styles.main}>
